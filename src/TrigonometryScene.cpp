6d2c79b1ae34f248abd7e2c82c0ec09f2b4ec34a
Blake245 <jwillden@student.neumont.edu>
2025-05-05 22:04:04 -0600

    Trig

--- src/TrigonometryScene.cpp
+++ src/TrigonometryScene.cpp
@@ -9,10 +9,69 @@ void TrigonometryScene::Update()
 {
 }
 
+float DegToRad(float degrees)
+{
+	return degrees * (PI / 180);
+}
+
+float RadToDeg(float radians)
+{
+	return radians * (180 / PI);
+}
+
+float Vector2Length(const Vector2& v)
+{
+	return sqrtf((v.x * v.x) + (v.y * v.y));
+}
+
+Vector2 Vector2Normalize(const Vector2& v)
+{
+	float l = Vector2Length(v);
+	if (l == 0) return Vector2{ 0, 0 };
+	return Vector2{ v.x / l, v.y / l };
+}
+
 void TrigonometryScene::Draw()
 {
 	m_camera->BeginMode();
-	DrawCircle(0, 0, 100, RED);
+	DrawGrid(10, 5, WHITE);
+	//this->DrawCircle(Vector2{ 0, 0 }, 1, RED);
+	float radius = 3;
+	float rate = 0.5f;
+	float time = (float)GetTime() * rate;
+
+	int steps = 50;
+
+	//Circle
+	for (int i = 0; i < steps; i++)
+	{
+		float theta = (i / (float)steps) * (2 * PI); //0 - 1 
+		float x = cosf(theta) * radius;
+		float y = sinf(theta) * radius;
+
+		this->DrawCircle(Vector2{ x, y }, 0.25, RED);
+	}
+
+	//sin / cos
+	for (float x = -9; x < 9; x += 0.2)
+	{
+		float theta = time + ((x / 18) * (2 * PI));
+		float c = cosf(theta) * radius;
+		float s = sinf(theta) * radius;
+
+		this->DrawCircle(Vector2{ x, c }, 0.25, GREEN);
+		this->DrawCircle(Vector2{ x, s }, 0.25, BLUE);
+	}
+
+	//Orbit
+	float theta = time;
+	float x = cosf(theta) * radius;
+	float y = sinf(theta) * radius;
+	DrawCircle(Vector2{ x, y }, 0.25, YELLOW);
+
+
+	float angle = atan2f(y, x);
+
 	m_camera->EndMode();
 }
 
--- src/scene.h
+++ src/scene.h
@@ -21,9 +21,11 @@ public:
 
 	void SetCamera(SceneCamera* camera) { m_camera = camera; }
 	SceneCamera* GetCamera() { return m_camera; }
-
 protected:
 	void DrawGrid(float slices, float thickness, const Color& color);
+	void DrawText(const std::string& text, const Vector2& world, int fontSize, Color color);
+	void DrawCircle(const Vector2& world, float radius, Color color);
+	void DrawLine(const Vector2& v1, const Vector2& v2, float thickness, Color color);
 
 protected:
 	int m_width{ 0 };
